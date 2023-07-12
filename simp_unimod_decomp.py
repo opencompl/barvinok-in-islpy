9d1a4942757f6dcc1ceceefc82fbd3a72c896070
Abhinav271828 <abhinav.m@research.iiit.ac.in>
2023-07-12 19:30:31 +0100

    Revert to rays repr

--- simp_unimod_decomp.py
+++ simp_unimod_decomp.py
@@ -4,41 +4,33 @@ import islpy as isl
 
 # For simplicial cones
 class Cone():
-    def __init__(self, coeffs, constants):
-        #              [d, d]  [d]
-        self.coeffs = coeffs
-        self.constants = constants
-        self.d = len(coeffs)
-
-        space = Space.set_alloc(Context(), nparam=0, dim=self.d)
-        set = BasicSet.universe(space)
-
-        for i in range(self.d):
-            cons = Constraint.alloc_inequality(space)
-            cons = cons.set_constant_val(self.constants[i])
-            for j, c in enumerate(self.coeffs[i]):
-                cons = cons.set_coefficient_val(isl.dim_type.set, j, c)
-                set = set.add_constraint(cons)
-
-        self.set = set
+    def __init__(self, vertex, rays, sign=1):
+        #              [d]     [d, d]
+        self.vertex = vertex
+        self.rays = rays
+        self.sign = sign
 
     def get_index(self):
         """Volume of parallelepiped"""
         det = np.linalg.det(self.rays)
         return (0.5 * det)
 
-    def get_rays(self): # TODO
+    def get_sample_point(self): # TODO
+        """
+        Use LLL and find
+        """
         pass
 
 def unimodular_decomp(cone):
     ind = cone.get_index()
-    if ind == 1: return [cone]
+    if ind == 1: return [(1, cone)]
     else:
         cones = []
-        rays = cone.get_rays()
-        w = cone.set.sample_point()
+        rays = cone.rays
+        w = cone.get_sample_point()
+        sign = 1                     # TODO
         for i in range(len(rays)):
-            ki = Cone(rays[:i] + [w] + rays[i+1:])   # TODO
+            ki = Cone(cone.vertex, rays[:i] + [w] + rays[i+1:], sign)
             cones.append(ki)
         final_nested = map(unimodular_decomp, cones)
         final = [cone for decomp in final_nested for cone in decomp]
