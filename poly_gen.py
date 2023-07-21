394214544ffbd6647111f04273b823e442ff6452
Abhinav271828 <abhinav.m@research.iiit.ac.in>
2023-07-21 19:52:05 +0100

    Add substitution and solving

--- poly_gen.py
+++ poly_gen.py
@@ -2,12 +2,15 @@ import numpy as np
 from numpy.linalg import inv, det
 from numpy import transpose, ceil
 import olll
-from math import gcd
+from math import gcd, factorial
 import cdd
+from functools import reduce
 
 def sign(x): return x/abs(x)
 
 def subAtWith(l, i, x): return (l[:i] + [x] + l[i+1:])
+
+def dot(a, b): return sum(ai*bi for ai,bi in zip(a,b))
 
 # For simplicial cones
 class Cone():
@@ -130,7 +133,6 @@ class GenFunc():
                     "".join([f"(1 - x^{list(d)})" for d in ds])
                         for s, n, ds in zip(self.signs, self.num_exps, self.den_exps)])
 
-
 def unimod_cone_gen_func(vertex, cone):
     if all(isinstance(i, int) for i in vertex): numerator = vertex
     else:
@@ -180,3 +182,78 @@ def polytope_gen_func(poly): # polyhedron in H-rep (Ax <= b)
 mat = cdd.Matrix([[1,-1,0,0],[0,1,0,0],[1,0,-1,0],[0,0,1,0],[1,0,0,-1],[0,0,0,1]])
 mat.rep_type = cdd.RepType.INEQUALITY
 poly = cdd.Polyhedron(mat)
+
+def comb(n, r):
+    numerator = reduce(lambda a, b: a*b, [n-i for i in range(0, r)], 1)
+    denominator = factorial(r)
+    return numerator/denominator
+
+def conv(l1, l2):
+    if len(l1) > len(l2): l2 += [0 for _ in range(len(l1)-len(l2))]
+    elif len(l2) > len(l1): l1 += [0 for _ in range(len(l2)-len(l1))]
+
+    l = len(l1)
+
+    return [sum(l1[i] * l2[j-i] for i in range(0,j+1))
+                for j in range(0,l)]
+
+def get_coeffs_numerator(num):
+    def p_of_s(i):
+        return comb(num, i)
+    return p_of_s
+
+def get_coeffs_denominator(dens):
+    coeffs_per_den = [[comb(den+1, k+1) for k in range(0, den+1)] for den in dens]
+    coeffs = reduce(conv, coeffs_per_den[1:], coeffs_per_den[0])
+    def q_of_s(i):
+        try: return coeffs[i]
+        except IndexError: return 0
+    return q_of_s
+
+def get_coeff_of(r, p, q):
+    if r == 0: return p(0)/q(0)
+    else:
+        t = p(r)
+        for i in range(1,r+1):
+            t -= q(i) * get_coeff_of(r-i, p, q)
+        return t/q(0)
+
+def substitute_with_one_vector(gf):
+    generators_all = [exp for d in gf.den_exps for exp in d]
+    mu = get_non_orth_vector(generators_all)
+
+    constant_term = 0
+    for s, n, ds in zip(gf.signs, gf.num_exps, gf.den_exps):
+        num = dot(n, mu)
+        dens = [dot(d, mu) for d in ds]
+
+        negative_exps = [d for d in dens if d < 0]
+        if len(negative_exps) % 2 == 1: s = -s
+        c = sum(negative_exps)
+
+        dens = [abs(d)-1 for d in dens]
+        r = len(dens)
+        if r % 2 == 1: s = -s
+        num -= c
+
+        # Now we have an expression of the form
+        # (s+1)^num /                => P(s)
+        # s^r * Π_r((1+s)^di - 1)/s  => s^r * Q(s)
+            # 1 ≤ i ≤ r; di = dens[i]+1
+
+        p_of_s = get_coeffs_numerator(num)
+        q_of_s = get_coeffs_denominator(dens)
+
+        constant_term += get_coeff_of(r, p_of_s, q_of_s)
+
+    return constant_term
+
+def get_non_orth_vector(vectors): # none of the vectors should be 0
+    dim = len(vectors[0])
+    new_vector = [1]
+    for d in range(2, dim+1):
+        lower_dim_dot_products = [dot(new_vector, v[:d-1]) for v in vectors]
+        disallowed_values = [-d/v[-1] for d, v in zip(lower_dim_dot_products, vectors) if v[-1] != 0]
+        new_coordinate = ceil(max(disallowed_values) + 1).astype(int).item()
+        new_vector.append(new_coordinate)
+    return new_vector
